ebb5e1c53919a4edee1957b3ca87d382edcc2cda
Neto <63604008+c13neto@users.noreply.github.com>
2026-09-04 21:29:20 -0300

    style: stylized menu burguer button

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
 export class Menu {
-  isOpen = signal(false);
-
+  isOpen = false;
   togglemenu(){
     this.isOpen = !this.isOpen;
   }
